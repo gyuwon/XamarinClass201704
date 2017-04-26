e62a7f3af79a05469eca427cf807847731a39877
Gyuwon <gyuwon@live.com>
2017-04-26 15:38:22 +0900

    이벤트 예제 코드를 추가합니다.

---
+++ CSharpFeatures/Events/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new ObservableUser("Tony Stark");
+
+            user.PropertyChanged += OnUserPropertyChanged;
+
+            user.Username = "Ironman";
+        }
+
+        private static void OnUserPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ObservableUser.Username))
+            {
+                var user = (ObservableUser)sender;
+                Console.WriteLine($"Hello {user.Username}");
+            }
+        }
+    }
+
+    public class ObservableUser : INotifyPropertyChanged
+    {
+        private string _username;
+
+        public ObservableUser(string username)
+        {
+            _username = username;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+
+                // 1. 다중 스레딩 취약
+                if (PropertyChanged != null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Username"));
+
+                // 2. 다중 스레딩 안전
+                ////var eventHandler = PropertyChanged;
+
+                ////if (eventHandler != null)
+                ////    eventHandler.Invoke(this, new PropertyChangedEventArgs("Username"));
+
+                // 3. Null 전파 사용
+                ////PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+    }
+}
