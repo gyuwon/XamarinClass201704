1dd90f8d596a2b12351100335fb0a713b00555ba
Gyuwon <gyuwon@live.com>
2017-04-26 16:13:56 +0900

    LINQ 예제 코드를 추가합니다.

---
+++ CSharpFeatures/LINQ/Controllers/TodoItemsController.cs
@@ -0,0 +1,136 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using LINQ.Models;
+
+namespace LINQ.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        private TodoListContext db;
+
+        public TodoItemsController()
+        {
+            db = new TodoListContext();
+            db.Database.Log = m => Debug.WriteLine(m);
+        }
+
+        // GET: TodoItems
+        public async Task<ActionResult> Index(bool includeCompleted = false)
+        {
+            IQueryable<TodoItem> query = from t in db.TodoItems
+                                         where includeCompleted || t.IsCompleted == false
+                                         orderby t.Id descending
+                                         select t;
+
+            return View(await query.ToListAsync());
+        }
+
+        // GET: TodoItems/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = await db.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TodoItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title,IsCompleted")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TodoItems.Add(todoItem);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = await db.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,IsCompleted")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todoItem).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = await db.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            TodoItem todoItem = await db.TodoItems.FindAsync(id);
+            db.TodoItems.Remove(todoItem);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CSharpFeatures/LINQ/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
