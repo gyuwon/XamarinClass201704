148825398323c2e352e781821e48ba42844fd036
Gyuwon Yi <gyuwon@live.com>
2017-04-26 21:34:42 +0900

    Xamarin iOS 응용프로그램을 추가합니다.

---
+++ XamarinNative/XamarinIOS/DetailViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinIOS
+{
+	public partial class DetailViewController : UIViewController
+	{
+		public object DetailItem { get; set; }
+
+		protected DetailViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public void SetDetailItem(object newDetailItem)
+		{
+			if (DetailItem != newDetailItem)
+			{
+				DetailItem = newDetailItem;
+
+				// Update the view
+				ConfigureView();
+			}
+		}
+
+		void ConfigureView()
+		{
+			// Update the user interface for the detail item
+			if (IsViewLoaded && DetailItem != null)
+				detailDescriptionLabel.Text = DetailItem.ToString();
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			ConfigureView();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
