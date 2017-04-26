202e7369599af9fc77b5c098cbe1c97320a34665
Gyuwon Yi <gyuwon@live.com>
2017-04-26 23:12:28 +0900

    PCL 크로스플랫폼 응용프로그램을 추가합니다.

---
+++ PCLCrossPlatform/Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+
+namespace PCLCrossPlatform.Droid
+{
+	[Activity(Label = "PCLCrossPlatform", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		private readonly CounterService _service = new CounterService();
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate
+			{
+				_service.Count();
+				button.Text = _service.Message;
+				button.SetTextColor(_service.ShouldMessageBeHighlighted ? Color.LightSalmon : Color.White);
+			};
+		}
+	}
+}
---
+++ PCLCrossPlatform/PCLCrossPlatform/CounterService.cs
@@ -0,0 +1,13 @@
+﻿namespace PCLCrossPlatform
+{
+	public class CounterService
+	{
+		private int _count = 0;
+
+		public string Message => $"{_count} clicks!";
+
+		public bool ShouldMessageBeHighlighted => _count % 5 == 0;
+
+		public void Count() => _count++;
+	}
+}
