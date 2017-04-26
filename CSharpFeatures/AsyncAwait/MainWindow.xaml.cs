9007f41980c21d61ad32eaef87344d33cf8b8fdc
Gyuwon <gyuwon@live.com>
2017-04-26 20:55:04 +0900

    async-await 예제 코드를 추가합니다.

---
+++ CSharpFeatures/AsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _clearResult_Click(object sender, RoutedEventArgs e)
+        {
+            _getResult.Text = string.Empty;
+        }
+
+        private void _get_Click(object sender, RoutedEventArgs e)
+        {
+            _progressIndicator.IsActive = true;
+
+            var response = _httpClient.GetAsync(_uri.Text).Result;
+
+            _getResult.Text = response.IsSuccessStatusCode
+                ? response.Content.ReadAsStringAsync().Result
+                : $"Error({response.StatusCode})";
+
+            _progressIndicator.IsActive = false;
+        }
+
+        private void _getTpl_Click(object sender, RoutedEventArgs e)
+        {
+            _progressIndicator.IsActive = true;
+
+            _httpClient.GetAsync(_uri.Text)
+                .ContinueWith((Task<HttpResponseMessage> t) =>
+                {
+                    var response = t.Result;
+                    return response.IsSuccessStatusCode
+                        ? response.Content.ReadAsStringAsync()
+                        : Task.FromResult($"Error({response.StatusCode})");
+                })
+                .Unwrap()
+                .ContinueWith((Task<string> t) =>
+                {
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        _getResult.Text = t.Result;
+                        _progressIndicator.IsActive = false;
+                    });
+                });
+        }
+
+        private async void _getAwait_Click(object sender, RoutedEventArgs e)
+        {
+            _progressIndicator.IsActive = true;
+
+            var response = await _httpClient.GetAsync(_uri.Text);
+
+            _getResult.Text = response.IsSuccessStatusCode
+                ? await response.Content.ReadAsStringAsync()
+                : $"Error({response.StatusCode})";
+
+            _progressIndicator.IsActive = false;
+        }
+    }
+}
